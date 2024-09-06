1b7478f39d36e1d7fa72c384db9495bb0a3cd6f5
Jordan Dullier <jordan.dullier29@gmail.com>
2024-09-06 15:01:50 +0200

    components setup

---
+++ src/app/dashboard/support-tickets/support-tickets.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import {NewTicketComponent} from "../tickets/new-ticket/new-ticket.component";
+
+@Component({
+  selector: 'app-support-tickets',
+  standalone: true,
+  imports: [
+    NewTicketComponent
+  ],
+  templateUrl: './support-tickets.component.html',
+  styleUrl: './support-tickets.component.css'
+})
+export class SupportTicketsComponent {
+
+}
