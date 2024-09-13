45dc5a585085c094c7225b3315fc66fcdc6b6e3b
Jordan Dullier <jordan.dullier29@gmail.com>
2024-09-13 15:33:03 +0200

    Add some dynamic with lifecycle methods

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit, OnDestroy} from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -7,6 +7,28 @@ import { Component } from '@angular/core';
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css'
 })
-export class ServerStatusComponent {
-  currentStatus = 'online';
+export class ServerStatusComponent implements OnInit, OnDestroy{
+  currentStatus: 'unknown' | 'online' | 'offline' = 'unknown';
+  private interval?: ReturnType<typeof setInterval>
+
+  constructor() {}
+
+  ngOnInit(){
+    this.interval =  setInterval(() => {
+      const random = Math.random()
+
+      if(random < 0.5){
+        this.currentStatus = 'online'
+      }else if(random < 0.9){
+        this.currentStatus = 'offline'
+      }else{
+        this.currentStatus = 'unknown'
+      }
+    },5000)
+  }
+
+  ngOnDestroy(){
+    clearInterval(this.interval)
+  }
+
 }
